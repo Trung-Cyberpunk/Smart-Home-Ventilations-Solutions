d4f1b0d14d73dc6e8b0e7d4bbacf3e27af697b2c
Trung-Cyberpunk <trungnguyen14061@gmail.com>
2023-04-04 16:33:32 +0700

    All

--- Js/main.js
+++ Js/main.js
@@ -12,12 +12,12 @@ let counter = 0;//Đặt giá trị vị trí slide
 
 const size = slide[0].clientWidth; // Lấy kích thước của slide 
 
-slides.style.transform = 'translateX(' + (-size * counter) + 'px)';//Dịch chuyển phần tử theo chiều ngang với giá trị của size lấy ban đầu
+slides.style.transform   = 'translateX(' + (-size * counter) + 'px)';//Dịch chuyển phần tử theo chiều ngang với giá trị của size lấy ban đầu
 
 //CLick -> next
 nextbtn.addEventListener('click', () => {
   if (counter >= slide.length - 1) counter = -1; // nếu counter lớn hơn số slide hiện tại thì trả về -1(giá trị vị trí slide ban đầu)
-    //Update hiệu ứng chuyển động mượt mà thêm
+  slides.style.transition = 'transform 0.5s ease-in-out'; //Update hiệu ứng chuyển động mượt mà thêm
   counter++;//tăng giá trị vị trí slide
   slides.style.transform = 'translateX(' + (-size * counter) + 'px)';//Di chuyển vị trí
   currentSlideElement.textContent = counter + 1;//Tăng giá trị của current-silde
