5860946d075d909e4f40e5e65673d7527092851d
Trung-Cyberpunk <trungnguyen14061@gmail.com>
2023-04-05 11:29:04 +0700

    All

--- Js/main.js
+++ Js/main.js
@@ -23,6 +23,8 @@ nextbtn.addEventListener('click', () => {
   currentSlideElement.textContent = counter + 1;//Tăng giá trị của current-silde
 });
 
+
+
 //CLick -> back
 backbtn.addEventListener('click', () => {
   if (counter <= 0) return;//Counter = 0 thì stop
@@ -43,4 +45,109 @@ backbtn.addEventListener('click', () => {
 //     counter = slide.length - counter;
 //     slides.style.transform = 'translateX(' + (-size * counter) + 'px)';
 //   }
+// });
+
+
+
+const question = document.querySelector(".question_container-item");
+const questions = document.querySelector(".roll_question");
+const questions_item = document.querySelectorAll(".question_item");
+
+const btnback_qs = document.querySelectorAll(".back_qs");
+const btnnext_qs = document.querySelectorAll(".next_qs");
+
+const btnBackALL = document.querySelector(".comback");
+
+const size_qs = questions_item[0].clientWidth
+
+
+let number = 0;
+
+questions.style.transform   = 'translateX(' + (-size_qs * number) + 'px)';
+
+// //Lưu trữ số trang slide 
+// // const current_QSElement = document.querySelector(".current_QS");
+
+// btnnext_qs[0].addEventListener('click', () => {
+//   number = 1;//giá trị vị trí slide
+//   questions.style.transition = 'transform 0.5s ease-in-out'; //Update hiệu ứng chuyển động mượt mà thêm
+//   questions.style.transform = 'translateX(' + (-size_qs * number) + 'px)';//Di chuyển vị trí
+//   // current_QSElement.textContent = number + 1;//Tăng giá trị của current-silde
+// });
+
+// btnnext_qs[1].addEventListener('click', () => {
+//   // if (number >= question.length - 1) number = -1; // nếu counter lớn hơn số slide hiện tại thì trả về -1(giá trị vị trí slide ban đầu)
+//   questions.style.transition = 'transform 0.5s ease-in-out'; //Update hiệu ứng chuyển động mượt mà thêm
+//   number++;//tăng giá trị vị trí slide
+//   questions.style.transform = 'translateX(' + (-size_qs * number) + 'px)';//Di chuyển vị trí
+//   // current_QSElement.textContent = number + 1;//Tăng giá trị của current-silde
+// });
+// btnnext_qs[2].addEventListener('click', () => {
+//   // if (number >= question.length - 1) number = -1; // nếu counter lớn hơn số slide hiện tại thì trả về -1(giá trị vị trí slide ban đầu)
+//   questions.style.transition = 'transform 0.5s ease-in-out'; //Update hiệu ứng chuyển động mượt mà thêm
+//   number++;//tăng giá trị vị trí slide
+//   questions.style.transform = 'translateX(' + (-size_qs * number) + 'px)';//Di chuyển vị trí
+//   // current_QSElement.textContent = number + 1;//Tăng giá trị của current-silde
+// });
+
+
+var allItem = document.querySelectorAll(".roll_question .question")
+
+var countt = allItem.length;
+
+for(let i = 0; i < countt; i++){
+  btnnext_qs[i].addEventListener('click', () => {
+    number = i + 1;
+    questions.style.transition = 'transform 0.5s ease-in-out';
+    questions.style.transform = 'translateX(' + ( - size_qs * number) + 'px)';
+  })
+  btnback_qs[i].addEventListener('click', () => {
+    if (number <= 0) return;//Counter = 0 thì stop
+    questions.style.transition = 'transform 0.5s ease-in-out';
+    number--;
+    questions.style.transform = 'translateX(' + (-size_qs * number) + 'px)';
+  });
+  
+  btnBackALL.addEventListener('click', () => {
+    questions.style.transition = 'transform 0.5s ease-in-out'; //Update hiệu ứng chuyển động mượt mà thêm
+    questions.style.transform = 'translateX(' + (size_qs - size_qs) + 'px)';//Di chuyển vị trí
+  });
+}
+
+// btnBackALL.addEventListener('click', () => {
+//   questions.style.transition = 'transform 0.5s ease-in-out'; //Update hiệu ứng chuyển động mượt mà thêm
+//   questions.style.transform = 'translateX(' + (questions_item[0].clientWidth) + 'px)';//Di chuyển vị trí
+// });
+
+
+// for(let i = counter; i >= 0 ; i--){
+//   btnback_qs[i].addEventListener('click', () => {
+//     number = --i;
+//     question.style.transition = 'transform 0.5s ease in-out';
+//     question.sytle.transform = 'translate(' + ( -size_qs * number) + 'px)';
+//   })
+// }
+
+//CLick -> back
+// btnback_qs[0].addEventListener('click', () => {
+//   if (number <= 0) return;//Counter = 0 thì stop
+//   questions.style.transition = 'transform 0.5s ease-in-out';
+//   number--;
+//   questions.style.transform = 'translateX(' + (-size_qs * number) + 'px)';
+//   // 
+// });
+
+// btnback_qs[1].addEventListener('click', () => {
+//   if (number <= 0) return;//Counter = 0 thì stop
+//   questions.style.transition = 'transform 0.5s ease-in-out';
+//   number--;
+//   questions.style.transform = 'translateX(' + (-size_qs * number) + 'px)';
+//   // 
+// });
+// btnback_qs[2].addEventListener('click', () => {
+//   if (number <= 0) return;//Counter = 0 thì stop
+//   questions.style.transition = 'transform 0.5s ease-in-out';
+//   number--;
+//   questions.style.transform = 'translateX(' + (-size_qs * number) + 'px)';
+//   // 
 // });
